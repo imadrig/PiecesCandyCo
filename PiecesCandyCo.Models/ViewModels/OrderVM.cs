cba9eecbd5cc7e0d601bfc3ebfd26665a6606971
Ingrid Madrigal <39933650+imadrig@users.noreply.github.com>
2023-11-23 22:36:09 -0500

    Beginning Stages of Order Management Feature - testing through Home page

---
+++ PiecesCandyCo.Models/ViewModels/OrderVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiecesCandyCo.Models.ViewModels
+{
+    public class OrderVM
+    {
+        public CustomerOrderDetail CustomerOrderDetail { get; set; }
+        public IEnumerable<CartOrderDetail> CartOrderDetail { get; set; }
+        
+
+
+    }
+}
---
+++ PiecesCandyCo/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using PiecesCandyCo.DataAccess.Repository.IRepository;
+using PiecesCandyCo.Models;
+using PiecesCandyCo.Models.ViewModels;
+
+namespace PiecesCandyCo.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details(int orderId)
+        {
+            OrderVM orderVM = new OrderVM()
+            {
+                CustomerOrderDetail = _unitOfWork.CustomerOrderDetail.Get(u => u.Id == orderId, includeProperties: "ApplicationUser"),
+                CartOrderDetail = _unitOfWork.CartOrderDetail.GetAll(u => u.CustomerOrderDetailId == orderId, includeProperties: "Product")
+            };
+            return View(orderVM);
+        }
+
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<CustomerOrderDetail> customerOrderDetails = _unitOfWork.CustomerOrderDetail.GetAll(includeProperties: "ApplicationUser").ToList();
+            return Json(new { data = customerOrderDetails });
+        }
+
+        #endregion
+    }
+}

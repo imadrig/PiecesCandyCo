a5c81a3e5578efa1c9c537afe281ae2b8aa183f1
Ingrid Madrigal <39933650+imadrig@users.noreply.github.com>
2023-11-28 21:12:12 -0500

    dbinitializer created - prep for Azure deployment

---
+++ PiecesCandyCo.DataAccess/DbInitializer/DbInitializer.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using PiecesCandyCo.DataAccess.Data;
+using PiecesCandyCo.Models;
+using PiecesCandyCo.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiecesCandyCo.DataAccess.DbInitializer
+{
+    public class DbInitializer : IDbInitializer
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly ApplicationDbContext _db;
+
+
+        public DbInitializer(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager, ApplicationDbContext db)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _db = db;
+        }
+
+        public void Initialize()
+        {
+            try
+            {
+                if (_db.Database.GetPendingMigrations().Count() > 0)
+                {
+                    _db.Database.Migrate();
+                }
+            }
+            catch(Exception ex)
+            {
+
+            }
+
+            if (!_roleManager.RoleExistsAsync(SD.Role_Customer).GetAwaiter().GetResult())
+            {
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Customer)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).GetAwaiter().GetResult();
+
+                _userManager.CreateAsync (new ApplicationUser
+                {
+                    UserName = "admin@piecescandy.co",
+                    Email = "admin@piecescandy.co",
+                    Name = "Pieces Candy Co. Admin",
+                    PhoneNumber = "5557370291",
+                    StreetAddress = "123 Street",
+                    City = "Somewhere",
+                    State = "ST",
+                    ZipCode = "82105",
+                }, "Pass123!").GetAwaiter ().GetResult();
+
+                ApplicationUser user = _db.ApplicationUsers.FirstOrDefault(u => u.Email == "admin@piecescandy.co");
+                _userManager.AddToRoleAsync(user, SD.Role_Admin).GetAwaiter().GetResult();
+            }
+            return;
+        }
+    }
+}
--- PiecesCandyCo/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ PiecesCandyCo/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -123,11 +123,7 @@ namespace PiecesCandyCo.Areas.Identity.Pages.Account
 
         public async Task OnGetAsync(string returnUrl = null)
         {
-            if (!_roleManager.RoleExistsAsync(SD.Role_Customer).GetAwaiter().GetResult())
-            {
-                _roleManager.CreateAsync(new IdentityRole(SD.Role_Customer)).GetAwaiter().GetResult();
-                _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).GetAwaiter().GetResult();
-            }
+            
 
             Input = new InputModel() {
                 RoleList = _roleManager.Roles.Select(x => x.Name).Select(i => new SelectListItem
